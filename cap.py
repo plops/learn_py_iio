84a25379125fbaca28bb6097e7f9d19da3958881
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-03-14 07:56:49 +0100

    collect references

--- cap.py
+++ cap.py
@@ -1,3 +1,6 @@
+# https://ez.analog.com/thread/97117-using-iiopy-with-m2k
+# https://mirrors.dotsrc.org/fosdem/2018/AW1.120/plutosdr.webm
+# https://wiki.analog.com/resources/tools-software/linux-software/fmcomms2_plugin
 import iio, struct, time
 
 print(iio.version)
@@ -10,13 +13,27 @@ uri = next(iter(ctxs),None)
 
 ctx = iio.Context(uri)
 
+
 print(ctx.name)
 
 print(ctx.attrs)
 
-#{'hw_model': 'Analog Devices PlutoSDR Rev.B (Z7010-AD9363)', 'usb,release': '2.0', 'ad9361-phy,xo_correction': '40000035', 'usb,idVendor': '0456', 'usb,vendor': 'Analog Devices Inc.', 'usb,serial': '104400b83991000b0d000f00bbd8642eff', 'usb,product': 'PlutoSDR (ADALM-PLUTO)', 'usb,idProduct': 'b673', 'fw_version': 'v0.26', 'hw_model_variant': '1', 'local,kernel': '4.9.0-10194-g40c2158', 'hw_serial': '104400b83991000b0d000f00bbd8642eff', 'ad9361-phy,model': 'ad9364'}
+#{'hw_model': 'Analog Devices PlutoSDR Rev.B (Z7010-AD9363)', 'usb,release': '2.0', 'ad9361-phy,xo_correction': '40000035', 'usb,idVendor': '0456', 'usb,vendor': 'Analog Devices Inc.', 'usb,product': 'PlutoSDR (ADALM-PLUTO)', 'usb,idProduct': 'b673', 'fw_version': 'v0.26', 'hw_model_variant': '1', 'local,kernel': '4.9.0-10194-g40c2158', 'hw_serial': '', 'ad9361-phy,model': 'ad9364'}
+
+print(ctx.devices)
+
+[print(dev.id + ' ' + dev.name) for dev in ctx.devices]
+
+# iio:device3 cf-ad9361-dds-core-lpc
+# iio:device1 ad9361-phy
+# iio:device4 cf-ad9361-lpc
+# iio:device2 xadc
+# iio:device0 adm1177
 
 
+[print(dev.name + ' ' + str(dev.channels)) for dev in ctx.devices]
 
+dev = ctx.find_device('cf-ad9361-lpc')
 
-
+c0 = dev.find_channel('voltage0',True)
+c1 = dev.find_channel('voltage1',True)
