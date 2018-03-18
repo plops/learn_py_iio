64d350f899e221ee27558e72585b0a949ef58b62
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-03-18 12:36:06 +0100

    at least 2 buffer related functions are not defined by autowrap.
    what is going on?

--- cap.lisp
+++ cap.lisp
@@ -3,7 +3,7 @@
 
 
 
-(ql:quickload :cl-autowrap)
+(ql:quickload :cl-autowrap/libffi)
 (defparameter *spec-path* (merge-pathnames "stage/learn_py_iio/"
 					   (user-homedir-pathname)))
 
@@ -26,7 +26,8 @@
 		    (".*")
 		    :include-definitions
 		    ("^iio.*"
-		     "^IIO.*")
+		     "^IIO.*"
+		    )
 		    :sysincludes '("/usr/include" "/usr/include/linux"
 				   "/usr/lib64/clang/3.9.1/include/"
 				   #+nil"/usr/lib64/gcc/x86_64-pc-linux-gnu/5.4.0/include")
@@ -51,6 +52,8 @@
 				   "x86_64-unknown-openbsd")
 		    :trace-c2ffi t)
 
+autowrap::*foreign-functions*
+
 
 #+nil
 ((defparameter *sctx* (iio-create-scan-context (cffi:null-pointer) 0))
@@ -60,8 +63,115 @@
 
  (defparameter *ctx* (iio-create-default-context)))
 
+(ql:quickload "cl-ppcre")
 
-(defparameter *ctx* (iio-create-context-from-uri "usb:1.4.5"))
-(defparameter *rx-stream-dev* (iio-context-find-device *ctx* "cf-ad9361-lpc"))
+(defparameter *out*
+ (with-output-to-string (s)
+   (sb-ext:run-program "/usr/bin/iio_info" '("-s") :output s)))
+
+(defparameter *uri* (elt (ppcre:all-matches-as-strings "usb:[^\]]*" *out*) 0))
+
+
+(defparameter *ctx* (iio-create-context-from-uri *uri*))
+
 "A_BALANCED"
-examples/ad9361-iiostream.c
+
+(defparameter *rx-phy* (iio-context-find-device *ctx* "ad9361-phy"))
+
+;; lo 2.4GHz
+(iio-channel-attr-write-longlong
+ (iio-device-find-channel *rx-phy*
+			  "altvoltage0"
+			  1)
+ "frequency"
+ (round 2.4e9))
+
+;; baseband rate 5MS/s
+(iio-channel-attr-write-longlong
+ (iio-device-find-channel *rx-phy*
+			  "voltage0"
+			  0)
+ "sampling_frequency"
+ (round 5e6))
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; Get the RX capture device structure ;;
+    ;; Get the IQ input channels	   ;;
+    ;; Enable I and Q channel		   ;;
+    ;; Create the RX buffer		   ;;
+    ;; Fill the buffer			   ;;
+    ;; Process samples			   ;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defparameter *dev* (iio-context-find-device *ctx* "cf-ad9361-lpc"))
+
+(defparameter *i* (iio-device-find-channel *dev*
+					    "voltage0"
+					    0))
+(defparameter *q* (iio-device-find-channel *dev*
+					    "voltage1"
+					    0))
+
+(iio-channel-enable *i*)
+(iio-channel-enable *q*)
+
+
+
+;; __api struct iio_buffer * iio_device_create_buffer (const struct iio_device *dev,
+;;        size_t samples_count, bool cyclic) 
+
+
+(cffi:defcfun ("iio_device_create_buffer" %create-buffer) :pointer
+  (dev :pointer)
+  (sample-count size-t #+nil :uint64)
+  (cyclic :uint8))
+
+
+(AUTOWRAP:DEFINE-FOREIGN-FUNCTION
+     '(IIO-DEVICE-CREATE-BUFFER "iio_device_create_buffer")
+     '(:POINTER (AUTOWRAP::STRUCT (IIO-BUFFER :BIT-SIZE 0 :BIT-ALIGNMENT 0)))
+     '((DEV
+        (:POINTER
+         (AUTOWRAP::STRUCT (IIO-DEVICE :BIT-SIZE 0 :BIT-ALIGNMENT 0))))
+       (SAMPLES-COUNT SIZE-T) (CYCLIC :UNSIGNED-CHAR)))
+
+(AUTOWRAP:DEFINE-FOREIGN-FUNCTION
+     '(IIO-DEVICE-CREATE-BUFFER "iio_device_create_buffer")
+     '(:POINTER)
+     '((DEV
+        :POINTER)
+       (SAMPLES-COUNT :uint64)
+       (CYCLIC :UNSIGNED-CHAR)))
+
+
+(gethash 'IIO-DEVICE-CREATE-BUFFER autowrap::*foreign-functions*)
+#+nil
+(defparameter *buf*
+  (iio-device-create-buffer *dev* 4096 0))
+
+(defparameter *buf*
+  (%create-buffer (iio-device-ptr *dev*) 4096 0))
+
+(iio-buffer-refill *buf*)
+
+(let ((p-inc (iio-buffer-step *buf*))
+      (p-enc (iio-buffer-end *buf*))
+      )
+  (loop for (setf p-dat (iio-buffer-first *buf* *i*))
+     while (< p-dat p-end)
+     do
+       (incf p-dat p-inc)
+       (incf t-dat p-inc)
+       (let ((i (cffi:mem-aref p-dat short 0))
+	     (q (cffi:mem-aref p-dat short 1))))))
+(iio-buffer-destroy *buf*)
+(iio-context-destroy *ctx*)
+
+#+nil
+(autowrap:with-alloc (rx '(:struct stream-cfg))
+  (setf stream-clear-))
+
+;; examples/ad9361-iiostream.c
+
+;; https://wiki.analog.com/university/tools/pluto/controlling_the_transceiver_and_transferring_data
